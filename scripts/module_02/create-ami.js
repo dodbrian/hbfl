19d147c90e0e771d4721a3a21fc96a1bbd5b261e
Denis Zimin <roydronov@gmail.com>
2019-04-13 19:45:00 +0200

    module02

--- scripts/module_02/create-ami.js
+++ scripts/module_02/create-ami.js
@@ -1,14 +1,24 @@
 // Imports
-// TODO: Import the aws-sdk
+const AWS = require('aws-sdk')
 
-// TODO: Configure region
+AWS.config.update({ region: 'eu-central-1' })
 
 // Declare local variables
-// TODO: Create an ec2 object
+const ec2 = new AWS.EC2()
 
-createImage('<imageid>', 'hamsterImage')
-.then(() => console.log('Complete'))
+createImage('i-07caeb64ec8318bf0', 'hamsterImage')
+  .then(() => console.log('Complete'))
 
-function createImage (seedInstanceId, imageName) {
-  // TODO: Implement AMI creation
+function createImage(seedInstanceId, imageName) {
+  const params = {
+    InstanceId: seedInstanceId,
+    Name: imageName
+  }
+
+  return new Promise((resolve, reject) => {
+    ec2.createImage(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
 }
--- scripts/module_02/create-ec2-instance.js
+++ scripts/module_02/create-ec2-instance.js
@@ -1,40 +1,106 @@
 // Imports
-// TODO: Import the aws-sdk
+const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-// TODO: Configure region
+AWS.config.update({ region: 'eu-central-1' })
 
 // Declare local variables
-// TODO: Create an ec2 object
-const sgName = 'hamster_sg'
-const keyName = 'hamster_key'
+const ec2 = new AWS.EC2()
+const sgName = 'hamster_sg2'
+const keyName = 'hamster_key2'
 
 // Do all the things together
 createSecurityGroup(sgName)
-.then(() => {
-  return createKeyPair(keyName)
-})
-.then(helpers.persistKeyPair)
-.then(() => {
-  return createInstance(sgName, keyName)
-})
-.then((data) => {
-  console.log('Created instance with:', data)
-})
-.catch((err) => {
-  console.error('Failed to create instance with:', err)
-})
+  .then(() => {
+    return createKeyPair(keyName)
+  })
+  .then(helpers.persistKeyPair)
+  .then(() => {
+    return createInstance(sgName, keyName)
+  })
+  .then((data) => {
+    console.log('Created instance with:', data)
+  })
+  .catch((err) => {
+    console.error('Failed to create instance with:', err)
+  })
 
 // Create functions
 
-function createSecurityGroup (sgName) {
-  // TODO: Implement sg creation & setting SSH rule
+function createSecurityGroup(sgName) {
+  const params = {
+    Description: sgName,
+    GroupName: sgName
+  }
+
+  return new Promise((resolve, reject) => {
+    ec2.createSecurityGroup(params, (err, data) => {
+      if (err) reject(err)
+      else {
+        const params = {
+          GroupId: data.GroupId,
+          IpPermissions: [
+            {
+              IpProtocol: 'tcp',
+              FromPort: 22,
+              ToPort: 22,
+              IpRanges: [
+                {
+                  CidrIp: '0.0.0.0/0'
+                }
+              ]
+            }, {
+              IpProtocol: 'tcp',
+              FromPort: 3000,
+              ToPort: 3000,
+              IpRanges: [
+                {
+                  CidrIp: '0.0.0.0/0'
+                }
+              ]
+            }
+          ]
+        }
+
+        ec2.authorizeSecurityGroupIngress(params, (err) => {
+          if (err) reject(err)
+          else resolve()
+        })
+      }
+    })
+  })
 }
 
-function createKeyPair (keyName) {
-  // TODO: Create keypair
+function createKeyPair(keyName) {
+  const params = {
+    KeyName: keyName
+  }
+
+  return new Promise((resolve, reject) => {
+    ec2.createKeyPair(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
 }
 
-function createInstance (sgName, keyName) {
-  // TODO: create ec2 instance
+function createInstance(sgName, keyName) {
+  const params = {
+    ImageId: 'ami-0cd50b796266d18e4',
+    InstanceType: 't2.micro',
+    KeyName: keyName,
+    MaxCount: 1,
+    MinCount: 1,
+    SecurityGroups: [
+      sgName
+    ],
+    UserData: 'IyEvYmluL2Jhc2gKc3VkbyBhcHQtZ2V0IHVwZGF0ZQpzdWRvIGFwdC1nZXQgLXkgaW5zdGFsbCBnaXQKZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9yeWFubXVyYWthbWkvaGJmbC5naXQKY2QgaGJmbApzdWRvIG5wbSBpCnN1ZG8gbnBtIHJ1biBzdGFydA=='
+  }
+
+  return new Promise((resolve, reject) => {
+    ec2.runInstances(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
 }
