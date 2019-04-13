64c3f4da62ff7c1c64edeccdb45c46bef9e4bee0
Denis Zimin <roydronov@gmail.com>
2019-04-13 23:12:47 +0200

    module3

--- scripts/module_03/create-launch-configuration.js
+++ scripts/module_03/create-launch-configuration.js
@@ -1,10 +1,10 @@
 const AWS = require('aws-sdk')
 const helpers = require('./helpers')
 
-AWS.config.update({ region: '/* TODO: add your region */' })
+AWS.config.update({ region: 'eu-central-1' })
 
 // Declare local variables
-// TODO: Create an autoscaling object
+const autoScaling = new AWS.AutoScaling()
 
 const lcName = 'hamsterLC'
 const roleName = 'hamsterLCRole'
@@ -12,9 +12,26 @@ const sgName = 'hamster_sg'
 const keyName = 'hamster_key'
 
 helpers.createIamRole(roleName)
-.then(profileArn => createLaunchConfiguration(lcName, profileArn))
-.then(data => console.log(data))
+  .then(profileArn => createLaunchConfiguration(lcName, profileArn))
+  .then(data => console.log(data))
 
-function createLaunchConfiguration (lcName, profileArn) {
-  // TODO: Create a launch configuration
+function createLaunchConfiguration(lcName, profileArn) {
+  const params = {
+    IamInstanceProfile: profileArn,
+    ImageId: 'ami-069e92f570e2d3b33',
+    InstanceType: 't2.micro',
+    LaunchConfigurationName: lcName,
+    KeyName: keyName,
+    SecurityGroups: [
+      sgName
+    ],
+    UserData: 'IyEvYmluL2Jhc2gKc3VkbyBhcHQtZ2V0IHVwZGF0ZQpzdWRvIGFwdC1nZXQgLXkgaW5zdGFsbCBnaXQKZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9yeWFubXVyYWthbWkvaGJmbC5naXQKY2QgaGJmbApzdWRvIG5wbSBpCnN1ZG8gbnBtIHJ1biBzdGFydA=='
+  }
+
+  return new Promise((resolve, reject) => {
+    autoScaling.createLaunchConfiguration(params, (err, data) => {
+      if (err) reject(err)
+      else resolve(data)
+    })
+  })
 }
